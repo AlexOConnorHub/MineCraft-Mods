163d9d0c51ba1c4bfd006c408051ca4c51feb7b5
AlexOConnorHub <43659595+AlexOConnorHub@users.noreply.github.com>
2020-12-25 05:04:45 -0500

    Got it working!

--- src/main/java/PainTrain/firstmod/firstmod.java
+++ src/main/java/PainTrain/firstmod/firstmod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package PainTrain.firstmod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,17 +19,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("firstmod")
+public class firstmod
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public firstmod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
         // Register the processIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
         // Register the doClientStuff method for modloading
@@ -49,12 +47,6 @@ public class ExampleMod
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
